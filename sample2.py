d44fdbdf3fac9739d38f84aab305cfc2d36d0720
Nishimoto Haruto <haruto@pc603.ei.sanken.osaka-u.ac.jp>
2018-04-26 17:22:48 +0900

    second commit

---
+++ sample2.py
@@ -0,0 +1,18 @@
+# -*-coding: utf-8 -*-
+
+
+# こいつはsample2です．
+
+
+if __name__ == '__main__':
+
+
+	print '文字を入れてください．'
+	input_word = raw_input('>> ')
+	print input_word
+	print 'Hello world?2'
+	print 'Hello world?2'
+	print 'Hello world?2'
+	print 'Hello world?2'
+	print 'Hello world?2'
+
