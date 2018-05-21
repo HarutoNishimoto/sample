9bf88af010388f823953420ae1b40819d9f7e648
Nishimoto Haruto <haruto@su2.ei.sanken.osaka-u.ac.jp>
2018-05-21 10:44:13 +0900

    5/21更新

--- sample2.py
+++ sample2.py
@@ -11,10 +11,6 @@ if __name__ == '__main__':
 	input_word = raw_input('>> ')
 	print input_word
 	print 'Hello world?2'
-	print 'Hello world?2'
-	print 'Hello world?2'
-	print 'Hello world?2'
-	print 'Hello world?2'
 
 	print '文字を入れてください．'
 	input_word = raw_input('>> ')
