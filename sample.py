c7df4ef37f7fec1fe872e2058db917e7d419c2c5
Nishimoto Haruto <haruto@Harutos-Mac-mini.local>
2019-08-01 11:32:25 +0900

    追記

--- sample.py
+++ sample.py
@@ -5,19 +5,14 @@
 if __name__ == '__main__':
 
 
-	print '文字を入れてください．'
+	print('文字を入れてください．')
 	input_word = raw_input('>> ')
 	print input_word
-	print 'Hello world!'
-	print 'Hello world!'
-	print 'Hello world!'
-	print 'Hello world!'
-	print 'Hello world!'
+	print('Hello world!')
+	print('Hello world!')
 
-	
-	print 'Hello world!'
-	print 'Hello world!'
-	print 'Hello world!'
-	print 'Hello world!'
+
+
+
 
 
